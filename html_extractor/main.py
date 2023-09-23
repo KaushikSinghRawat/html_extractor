10e7546af45c4c8f327e25db904ae426c35b3038
KaushikSinghRawat <119755111+KaushikSinghRawat@users.noreply.github.com>
2023-09-23 13:15:33 +0530

    Create main.py

---
+++ html_extractor/main.py
@@ -0,0 +1,7 @@
+
+
+
+website_link = input('Website Link to be extracted: ')
+file_name = input('Name of file with file extension (default = data.txt): ')
+file_location = input('Location to save extracted data (default = current directory): ')
+character_encoding = input('Character Encoding (default = utf-8): ')
