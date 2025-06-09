9744387397b4cd5be537028bd90aa30603340f53
Kaushik Singh Rawat <119755111+KaushikSinghRawat@users.noreply.github.com>
2025-06-09 22:41:36 +0530

    Update html_request.py

--- html_extractor/request_maker/html_request.py
+++ html_extractor/request_maker/html_request.py
@@ -4,6 +4,6 @@ def html_request(link_address):
     return get(link_address).content
 
 
-link_address = "https://en.m.wikipedia.org/wiki/List_of_space_travellers_by_first_flight#Table" #link from whom html is to be extracted
+link_address = "https://en.wikipedia.org/wiki/Python_(programming_language)" #link from whom html is to be extracted
 
 request = html_request(link_address)
